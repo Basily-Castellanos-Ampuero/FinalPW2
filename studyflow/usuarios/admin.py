1ec05f7fd4cfefa4dc825490505d541d4761c7f2
Basily-Castellanos-Ampuero <bcastellanos@unsa.edu.pe>
2025-07-16 04:14:37 -0500

    Usuario admi

--- studyflow/usuarios/admin.py
+++ studyflow/usuarios/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Usuario
 
-# Register your models here.
+class UsuarioAdmin(BaseUserAdmin):
+    ordering = ['email']
+    list_display = ['email', 'nombre', 'is_staff', 'is_superuser']
+    search_fields = ['email', 'nombre']
+    fieldsets = (
+        (None, {'fields': ('email', 'nombre', 'password')}),
+        ('Permisos', {'fields': ('is_staff', 'is_superuser', 'is_active', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'nombre', 'password1', 'password2'),
+        }),
+    )
+    filter_horizontal = ('groups', 'user_permissions',)
+
+admin.site.register(Usuario, UsuarioAdmin)
