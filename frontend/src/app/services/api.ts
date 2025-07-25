a15042c1ca04a9da719a976dd1196ee16f68b4ac
Basily-Castellanos-Ampuero <bcastellanos@unsa.edu.pe>
2025-07-25 05:23:22 -0500

    actualizacion de api.ts

--- frontend/src/app/services/api.ts
+++ frontend/src/app/services/api.ts
@@ -11,9 +11,37 @@ export class ApiService {
 
   registrarUsuario(data: any): Observable<any> {
     return this.http.post(this.BASE_URL + 'registro/', data);
+    console.log('Enviando login:', data);
   }
 
   loginUsuario(data: any): Observable<any> {
+    console.log('Enviando login:', data);
     return this.http.post(this.BASE_URL + 'login/', data);
+    console.log('Enviando login:', data);
+
   }
+  obtenerPerfil(): Observable<any> {
+    return this.http.get('http://127.0.0.1:8000/api/usuarios/perfil/');
+  }
+  actualizarPerfil(data: any): Observable<any> {
+    return this.http.put('http://127.0.0.1:8000/api/usuarios/perfil/', data);
+  }
+  crearCurso(data: any): Observable<any> {
+    return this.http.post('http://127.0.0.1:8000/api/cursos/crear/', data);
+  }
+  obtenerCursosDelUsuario(): Observable<any> {
+    return this.http.get('http://127.0.0.1:8000/api/cursos/mis-cursos/');
+  }
+  obtenerCursoDetalle(cursoId: number): Observable<any> {
+    return this.http.get(`http://127.0.0.1:8000/api/cursos/detalle/${cursoId}/`);
+  }
+  
+  actualizarTema(temaId: number, data: any): Observable<any> {
+    return this.http.put(`http://127.0.0.1:8000/api/temas/tema/${temaId}/editar/`, data);
+  }
+
+  crearTema(cursoId: number, data: any): Observable<any> {
+    return this.http.post(`http://127.0.0.1:8000/api/temas/crear/${cursoId}/`, data);
+  }
+
 }
