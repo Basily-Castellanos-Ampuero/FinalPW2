24c0b020c043cd47ae8db6a415eb4639eecc5b84
Basily-Castellanos-Ampuero <bcastellanos@unsa.edu.pe>
2025-07-25 05:27:40 -0500

    rutas añadidas para las paginas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
-import { AuthComponent } from './pages/auth/auth.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { CursoComponent } from './pages/curso/curso.component';
+import { MisCursosComponent } from './pages/mis-cursos/mis-cursos.component';
+import { CursoDetalleComponent } from './pages/detalle-curso/detalle-curso.component';
+
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
-  { path: 'auth', component: AuthComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'perfil', loadComponent: () => import('./pages/perfil/perfil.component').then(m => m.PerfilComponent) },
+  { path: 'curso', component: CursoComponent },
+  { path: 'mis-cursos', component: MisCursosComponent },
+  { path: 'curso/:id', component: CursoDetalleComponent },
 ];
