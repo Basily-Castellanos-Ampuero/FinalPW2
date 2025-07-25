55a45ae56172cf9e4257e96747d1b579f92c41c4
Basily-Castellanos-Ampuero <bcastellanos@unsa.edu.pe>
2025-07-25 05:14:06 -0500

    actualizacion, por si es necesaria

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app';
-import { appConfig } from './app/app.config';
+import {
+  provideHttpClient,
+  withInterceptors,
+  withFetch
+} from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { AuthInterceptor } from './app/auth.interceptor';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withFetch(), 
+      withInterceptors([AuthInterceptor]) 
+    )
+  ]
+});
