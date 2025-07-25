51a2dfe5291b2938cd021410143526baa3ccc5f0
Fernando-Solsol <fsolsol@unsa.edu.pe>
2025-07-24 21:46:40 -0500

    Actualizando el urls.py

--- studyflow/usuarios/urls.py
+++ studyflow/usuarios/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegistroUsuarioView, LoginUsuarioView
+from .views import RegistroUsuarioView, LoginUsuarioView, PerfilUsuarioView
 
 urlpatterns = [
-    path('registro/', RegistroUsuarioView.as_view(), name='registro'),
-    path('login/', LoginUsuarioView.as_view(), name='login'),
+    path('registro/', RegistroUsuarioView.as_view()),
+    path('login/', LoginUsuarioView.as_view()),
+    path('perfil/', PerfilUsuarioView.as_view()),  
 ]
