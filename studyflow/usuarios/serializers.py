d2bd985ee22e987ffcd4afadd79ec888ff234cf6
Fernando-Solsol <fsolsol@unsa.edu.pe>
2025-07-24 21:45:55 -0500

    Actualizando serializers.py

--- studyflow/usuarios/serializers.py
+++ studyflow/usuarios/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from .models import Usuario
+from rest_framework import serializers
+from .models import Usuario
+
 
 class UsuarioRegistroSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -9,9 +12,15 @@ class UsuarioRegistroSerializer(serializers.ModelSerializer):
         fields = ['id', 'email', 'nombre', 'password']
 
     def create(self, validated_data):
-        user = Usuario.objects.create_user(
+        user = Usuario(
             email=validated_data['email'],
-            nombre=validated_data['nombre'],
-            password=validated_data['password']
+            nombre=validated_data['nombre']
         )
+        user.set_password(validated_data['password'])
+        user.save()
         return user
+    
+class UsuarioPerfilSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ['id', 'nombre', 'email']  # Añade más si tienes más campos
