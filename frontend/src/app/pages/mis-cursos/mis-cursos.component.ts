32fb0079c8a43034a72ff7fe6165d446f7eb6e92
Basily-Castellanos-Ampuero <bcastellanos@unsa.edu.pe>
2025-07-28 09:05:10 -0500

    delete añadido

--- frontend/src/app/pages/mis-cursos/mis-cursos.component.ts
+++ frontend/src/app/pages/mis-cursos/mis-cursos.component.ts
@@ -8,18 +8,35 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   selector: 'app-mis-cursos',
   templateUrl: './mis-cursos.component.html',
+  styleUrls: ['./mis-cursos.component.css'],
   imports: [CommonModule, RouterModule]
 })
 export class MisCursosComponent implements OnInit {
   cursos: any[] = [];
+  mensaje = ''; 
 
   constructor(private api: ApiService, private router: Router) {}
 
   ngOnInit() {
+    this.obtenerCursos(); 
+  }
+
+  obtenerCursos() {
     this.api.obtenerCursosDelUsuario().subscribe({
       next: (res) => this.cursos = res,
-      error: () => console.error('Error al obtener los cursos'),
+      error: () => this.mensaje = 'Error al obtener los cursos'
     });
-    console.log(this.cursos);
+  }
+
+  eliminarCurso(id: number) {
+    if (confirm('¿Estás seguro de que deseas eliminar este curso?')) {
+      this.api.eliminarCurso(id).subscribe({
+        next: () => {
+          this.mensaje = 'Curso eliminado correctamente.';
+          this.obtenerCursos(); // recargar lista
+        },
+        error: () => this.mensaje = 'No se pudo eliminar el curso.'
+      });
+    }
   }
 }
