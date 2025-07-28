3e6cd0cb99cbe81026617d80e2d6b788f4b7ab0b
Basily-Castellanos-Ampuero <bcastellanos@unsa.edu.pe>
2025-07-28 06:03:16 -0500

    Update urls.py para eliminacion

--- studyflow/cursos/urls.py
+++ studyflow/cursos/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import CursoCreateView, MisCursosView, CursoDetalleView, TemaCreateView, TemaUpdateView
+from .views import CursoCreateView, MisCursosView, CursoDetalleView, TemaCreateView, TemaUpdateView, CursoEliminarView
 
 urlpatterns = [
     path('crear/', CursoCreateView.as_view()),
     path('mis-cursos/', MisCursosView.as_view()),
     path('detalle/<int:curso_id>/', CursoDetalleView.as_view()),
-
+    path('eliminar/<int:pk>/', CursoEliminarView.as_view(), name='eliminar_curso'),
 ]
 
