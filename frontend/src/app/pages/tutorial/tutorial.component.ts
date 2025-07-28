668a11bbe0cf3b1f5caa4b56745cb5100345b68e
Basily-Castellanos-Ampuero <bcastellanos@unsa.edu.pe>
2025-07-28 09:02:23 -0500

    pagina del tutorial

---
+++ frontend/src/app/pages/tutorial/tutorial.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tutorial',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tutorial.component.html',
+})
+export class TutorialComponent {}
